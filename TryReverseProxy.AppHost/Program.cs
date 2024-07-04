014e4ec38cdb0289a03358768da4f3a3c5c27393
j108251113 <j108251113@nkust.edu.tw>
2024-07-04 22:27:09 +0800

    新增反向代理專案及 YARP 依賴

--- TryReverseProxy.AppHost/Program.cs
+++ TryReverseProxy.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.TryReverseProxy>("tryreverseproxy");
 
+builder.AddProject<Projects.ReverseProxy>("reverseproxy");
+
 builder.Build().Run();
