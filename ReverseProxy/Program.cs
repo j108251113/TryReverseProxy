cb0cc6849a5b9fc4482851b8a143f62df7c76d64
j108251113 <j108251113@nkust.edu.tw>
2024-07-04 22:49:23 +0800

    新增載入共用設定檔及環境設定檔邏輯，並設定設定檔的基本路徑

--- ReverseProxy/Program.cs
+++ ReverseProxy/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
+builder.Configuration
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile($"appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
 
 // Add services to the container.
 
