20a501d654f9b86e86d05681906c46bdea40f4cf
j108251113 <j108251113@nkust.edu.tw>
2024-07-04 22:51:46 +0800

    複製貼上黑暗執行緒的壹讀範例程式碼

--- ReverseProxy/Program.cs
+++ ReverseProxy/Program.cs
@@ -1,3 +1,7 @@
+using System.IO.Compression;
+using System.Text;
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
@@ -5,37 +9,59 @@ builder.Configuration
     .AddJsonFile($"appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
 
-// Add services to the container.
-
-var app = builder.Build();
-
-app.MapDefaultEndpoints();
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+// 原始碼出處：https://blog.darkthread.net/blog/yarp-lab/
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddTransforms(context => {
+        context.AddResponseTransform(async respCtx => {
+            var resp = respCtx.HttpContext.Response;
+            var req = respCtx.HttpContext.Request;
+            var srcResp = respCtx.ProxyResponse;
+            if ((srcResp?.Content.Headers.ContentType?.MediaType ?? string.Empty).Contains("html")) {
+                var stream = await srcResp?.Content.ReadAsStreamAsync()!;
+                // Decompress the response stream if needed
+                if (srcResp.Content.Headers.ContentEncoding.Any()) {
+                    resp.Headers.Remove("Content-Encoding");
+                    stream = new GZipStream(stream, CompressionMode.Decompress);
+                }
+                using var reader = new StreamReader(stream);
+                // TODO: size limits, timeouts
+                var body = await reader.ReadToEndAsync();
+                if (!string.IsNullOrEmpty(body)) {
+                    respCtx.SuppressResponseBody = true;
+                    // 停用廣告相關 JavaScript
+                    body = body.Replace("src=\"//pagead2", "_=\"");
+                    body = body.Replace("src=\"https://pagead2", "_=\"");
+                    body = body.Replace("src=\"https://player.gliacloud.com", "_=\"");
+                    // 將圖片連結轉換為本地代理、移除廣告區塊
+                    body = body.Replace("</body>", @"<script>
+document.querySelectorAll('img[data-src]').forEach(img => {
+    let src = img.getAttribute('data-src'); // 原程式採 Lazy-Loading，檢視到才下載，URL 在 data-src
+    img.setAttribute('data-src', src.replace(/https:\/\/([^.]+).read01.com/, '/imgbed-$1'));
 });
-
+document.querySelectorAll('.axslot').forEach(o => o.remove());
+</script></body>");
+                    var bytes = Encoding.UTF8.GetBytes(body);
+                    resp.ContentLength = bytes.Length;
+                    await respCtx.HttpContext.Response.Body.WriteAsync(bytes);
+                }
+            }
+        });
+    });
+var app = builder.Build();
+// 原始碼出處：https://blog.darkthread.net/blog/yarp-lab/
+// 處理圖床照片下載
+app.MapGet("/imgbed-{hostName}/{**path}", async (HttpContext ctx, string hostName, string path) => {
+    var url = $"https://{hostName}.read01.com/{path}";
+    var client = new HttpClient();
+    var resp = await client.GetAsync(url);
+    if (resp.IsSuccessStatusCode) {
+        var contentType = resp.Content.Headers.ContentType!.ToString();
+        ctx.Response.ContentType = contentType;
+        await resp.Content.CopyToAsync(ctx.Response.Body);
+    } else {
+        ctx.Response.StatusCode = (int)resp.StatusCode;
+    }
+});
+app.MapReverseProxy();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
